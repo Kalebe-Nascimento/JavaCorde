88e32f88e9b0a8c96c7b1601cdc9a00c08eda49a
Kalebe-Nascimento <kalebeinfo@gmail.com>
2025-04-01 15:22:22 -0300

    first endpoint

---
+++ src/main/java/com/javAcorde/poo/JavAcordeApplication.java
@@ -0,0 +1,13 @@
+package com.javAcorde.poo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavAcordeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavAcordeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javAcorde/poo/JavAcordeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javAcorde.poo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavAcordeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
